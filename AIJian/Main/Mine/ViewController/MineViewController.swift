548ae11aa2b59be869eb9ed538e243bf1c7c072f
zhuozuozhang <1115824104@qq.com>
2019-07-23 21:13:50 +0800

    20190723

--- AIJian/Main/Mine/ViewController/MineViewController.swift
+++ AIJian/Main/Mine/ViewController/MineViewController.swift
@@ -13,10 +13,39 @@ class MineViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let view = AJMineHeaderView(frame: CGRect(x: <#T##CGFloat#>, y: <#T##CGFloat#>, width: <#T##CGFloat#>, height: <#T##CGFloat#>))
+        //将tableView加入到整个视图当中
+        self.view.addSubview(self.tableView)
         // Do any additional setup after loading the view.
     }
     
+    //列表数据
+    private lazy var dataSource: Array = {
+        return [[["icon":"video", "title": "信息管理"],
+                 ["icon":"video", "title": "清楚缓存"],
+                 ["icon":"video", "title": "密码修改"],
+                 ["icon":"video", "title": "血糖设置"]],
+                
+                [["icon":"video", "title": "视频帮助"],
+                 ["icon":"video", "title": "关于我们"],
+                ["icon":"video", "title": "版本更新"]]
+            ]
+    }()
+    
+    // 懒加载顶部部分视图
+    private lazy var headerView:AJMineHeaderView = {
+        let view = AJMineHeaderView.init(frame: CGRect(x:0, y:0, width:AJScreenWidth, height: 300))
+        view.delegate = self
+        return view
+    }()
 
+    //懒加载TableView
+    private lazy var tableView : UITableView = {
+        let tableView = UITableView.init(frame:CGRect(x:0, y:0, width:AJScreenWidth, height:AJScreenHeight), style: UITableView.Style.plain)
+       tableView.delegate = self
+       tableView.dataSource = self
+       return tableView
+    }()
     /*
     // MARK: - Navigation
 
@@ -28,3 +57,37 @@ class MineViewController: UIViewController {
     */
 
 }
+//tableView Delegate
+extension MineViewController : UITableViewDelegate,UITableViewDataSource{
+    
+    //有几个大部分
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    //每个部分有几个
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 1{
+            return 4
+        }
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    //有几个大部分
+    
+    
+    
+}
+
+ //设置顶部部分视图的代理
+extension MineViewController : AJMineHeaderViewDelegate{
+    
+    
+    
+}
+
+
--- AIJian/const.swift
+++ AIJian/const.swift
@@ -10,14 +10,14 @@ import Foundation
 import UIKit
 
 let AJScreenWidth = UIScreen.main.bounds.width
-let AJScrennHeight = UIScreen.main.bounds.height
+let AJScreenHeight = UIScreen.main.bounds.height
 //白色
 let DominantColor = UIColor.init(red: 0, green: 0, blue: 0, alpha: 1)
 //黑色
 let FooterViewColor = UIColor.init(red: 240/255.0, green: 241/255.0, blue: 244/255.0, alpha: 1)
 
 // iphone X
-let isIphoneX = AJScrennHeight == 812 ? true : false
+let isIphoneX = AJScreenHeight == 812 ? true : false
 // navigationBarHeight
 let navigationBarHeight : CGFloat = isIphoneX ? 88 : 64
 // tabBarHeight
