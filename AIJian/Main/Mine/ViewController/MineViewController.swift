88ba43200f47b55daf132513d642ee8ad3e4c500
zhuozuozhang <1115824104@qq.com>
2019-07-29 19:37:27 +0800

    我的修改

--- AIJian/Main/Mine/ViewController/MineViewController.swift
+++ AIJian/Main/Mine/ViewController/MineViewController.swift
@@ -1,4 +1,4 @@
-//
+
 //  MineViewController.swift
 //  AIJian
 //
@@ -9,22 +9,26 @@
 import UIKit
 
 class MineViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let headview = AJMineHeaderView(frame: CGRect(x: 0, y: navigationBarHeight, width: AJScreenWidth, height: AJScreenHeight/3))
+        self.view.addSubview(headview)
+      
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    //列表数据
+    private lazy var dataSource: Array = {
+        return [[["icon":"video", "title": "信息管理"],
+                 ["icon":"video", "title": "清楚缓存"],
+                 ["icon":"video", "title": "密码修改"],
+                 ["icon":"video", "title": "血糖设置"]],
+                
+                [["icon":"video", "title": "视频帮助"],
+                 ["icon":"video", "title": "关于我们"],
+                 ["icon":"video", "title": "版本更新"]]
+        ]
+    }()
+    
 }
